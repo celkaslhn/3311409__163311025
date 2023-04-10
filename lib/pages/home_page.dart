f379e12d1450f113549e0426b9118d874a738a0f
celkaslhn <163311025@ogr.selcuk.edu.tr>
2023-04-10 13:57:15 +0300

    mobil vize

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:habittrackertute/components/habit_tile.dart';
+import 'package:habittrackertute/components/month_summary.dart';
+import 'package:habittrackertute/components/my_fab.dart';
+import 'package:habittrackertute/components/my_alert_box.dart';
+import 'package:habittrackertute/data/habit_database.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  HabitDatabase db = HabitDatabase();
+  final _myBox = Hive.box("Habit_Database");
+
+  @override
+  void initState() {
+    // mevcut bir alışkanlık listesi yoksa, uygulamayı ilk kez açıyorsunuz
+    // ardından varsayılan verileri oluştur
+    if (_myBox.get("CURRENT_HABIT_LIST") == null) {
+      db.createDefaultData();
+    }
+
+// zaten veri var, bu ilk değil
+    else {
+      db.loadData();
+    }
+
+    // update the database
+    db.updateDatabase();
+
+    super.initState();
+  }
+
+  // checkbox was tapped
+  void checkBoxTapped(bool? value, int index) {
+    setState(() {
+      db.todaysHabitList[index][1] = value;
+    });
+    db.updateDatabase();
+  }
+
+  // create a new habit
+  final _newHabitNameController = TextEditingController();
+  void createNewHabit() {
+    // kullanıcının yeni alışkanlık ayrıntılarını girmesi için uyarı iletişim kutusunu göster
+    showDialog(
+      context: context,
+      builder: (context) {
+        return MyAlertBox(
+          controller: _newHabitNameController,
+          hintText: 'Enter habit name..',
+          onSave: saveNewHabit,
+          onCancel: cancelDialogBox,
+        );
+      },
+    );
+  }
+
+  // save new habit
+  void saveNewHabit() {
+// bugünün alışkanlık listesine yeni bir alışkanlık ekle
+    setState(() {
+      db.todaysHabitList.add([_newHabitNameController.text, false]);
+    });
+
+    // clear textfield
+    _newHabitNameController.clear();
+    // pop dialog box
+    Navigator.of(context).pop();
+    db.updateDatabase();
+  }
+
+  // cancel new habit
+  void cancelDialogBox() {
+    // clear textfield
+    _newHabitNameController.clear();
+
+    // pop dialog box
+    Navigator.of(context).pop();
+  }
+
+  // düzenlemek için alışkanlık ayarlarını aç
+  void openHabitSettings(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return MyAlertBox(
+          controller: _newHabitNameController,
+          hintText: db.todaysHabitList[index][0],
+          onSave: () => saveExistingHabit(index),
+          onCancel: cancelDialogBox,
+        );
+      },
+    );
+  }
+
+// mevcut alışkanlığı yeni bir adla kaydet
+  void saveExistingHabit(int index) {
+    setState(() {
+      db.todaysHabitList[index][0] = _newHabitNameController.text;
+    });
+    _newHabitNameController.clear();
+    Navigator.pop(context);
+    db.updateDatabase();
+  }
+
+  // delete habit
+  void deleteHabit(int index) {
+    setState(() {
+      db.todaysHabitList.removeAt(index);
+    });
+    db.updateDatabase();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[300],
+      floatingActionButton: MyFloatingActionButton(onPressed: createNewHabit),
+      body: ListView(
+        children: [
+          // monthly summary heat map
+          MonthlySummary(
+            datasets: db.heatMapDataSet,
+            startDate: _myBox.get("START_DATE"),
+          ),
+
+          // list of habits
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: db.todaysHabitList.length,
+            itemBuilder: (context, index) {
+              return HabitTile(
+                habitName: db.todaysHabitList[index][0],
+                habitCompleted: db.todaysHabitList[index][1],
+                onChanged: (value) => checkBoxTapped(value, index),
+                settingsTapped: (context) => openHabitSettings(index),
+                deleteTapped: (context) => deleteHabit(index),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
